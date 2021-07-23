80f3930eb375f892e98cdd83ee39e04953028dfb
Amanda <amandaben21@aol.com>
2021-07-23 01:09:00 -0400

    commit tree fc5be76d9a60026261af182d8b1a1dce32da999a

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,44 @@
+let state= {count: 0};
+
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
       return {count: state.count + 1}
-    default:
-      return state;
-  }
-}
+      default:
+        return state;
+      }
+    }
+    
+    function dispatch(action){
+      state = changeState(state, action)
+      render()
+    }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
-changeState(state, action)
+    function render(){
+      document.body.textContent= state.count
+    }
+    render()
+    
+    dispatch({type: 'INCREASE_COUNT'})
+    dispatch({type: 'INCREASE_COUNT'})
+    dispatch({type: 'INCREASE_COUNT'})
+    dispatch({type: 'INCREASE_COUNT'})
+    
+    
+    
+    
+    
+    
+    //let state = {count: 0}
+    //let action = {type: 'INCREASE_COUNT'}
+    
+    //changeState(state, action)
+    
+    //console.log(changeState(state, {type: 'INCREASE_COUNT'}))
+    //console.log(changeState(state, {type: 'INCREASE_COUNT'})) --> It starts off as  zero, but when return it doesn't go beyond 1
+    
+    //console.log(state= changeState(state, {type: 'INCREASE_COUNT'}))
+    //console.log(state= changeState(state, {type: 'INCREASE_COUNT'})) -->ressigning state to the return value of reducer. The second time called on changeState it will update state in its arguement
+    
+    //console.log(dispatch({type: 'INCREASE_COUNT'}))
+    //console.log(dispatch({type: 'INCREASE_COUNT'}))
